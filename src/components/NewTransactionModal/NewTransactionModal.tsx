719c3ce1ae7de8c6bcd568658e87da0dea92f95e
gabrielforster <rochafrgabriel@gmail.com>
2022-05-20 21:31:44 -0300

    feat: styling modal

--- src/components/NewTransactionModal/NewTransactionModal.tsx
+++ src/components/NewTransactionModal/NewTransactionModal.tsx
@@ -1,5 +1,11 @@
 
+import {useState} from 'react'
 import Modal from "react-modal";
+import {Container,TransactionTypeContainer,TransactionTypeSelector} from './styles'
+
+import closeImg from '../../assets/close.svg'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
 
 interface NewTransactionModalProps{
   isOpen: boolean,
@@ -7,10 +13,61 @@ interface NewTransactionModalProps{
 }
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
+
+  const[transactionType, setTransactionType] = useState('deposit')
+
   return (
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
-    />
+      overlayClassName='react-modal-overlay'
+      className='react-modal-content'
+    >
+      <button type="button" onClick={onRequestClose} className='react-modal-close'>
+        <img src={closeImg} alt="Fechar Modal" />
+      </button>
+
+      <Container>
+        <h2>Cadastrar nova transação</h2>
+
+        <input
+          placeholder="Título"
+        />
+
+        <input
+          type="number"
+          placeholder="Valor"
+        />
+
+        <TransactionTypeContainer>
+          <TransactionTypeSelector
+            type="button"
+            onClick={()=>setTransactionType('deposit')}
+            isActive={transactionType === 'deposit'}
+            activeColor={'deposit'}
+          >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </TransactionTypeSelector>
+
+          <TransactionTypeSelector
+            type="button"
+            onClick={()=>setTransactionType('withdraw')}
+            isActive={transactionType === 'withdraw'}
+            activeColor={'withdraw'}
+          >
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </TransactionTypeSelector>
+        </TransactionTypeContainer>
+
+        <input
+          placeholder="Categoria"
+        />
+
+        <button type="submit">Cadastrar</button>
+
+      </Container>
+    </Modal>
   );
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,12 +5,12 @@ export const GlobalStyle = createGlobalStyle`
      --red: #e52e4d;
      --blue: #5429cc;
      --green: #33cc95;
-     
+
      --blue-light: #6933ff;
-     
+
      --text-title: #363f5f;
      --text-body: #969cb3;
-     
+
      --background: #f0f2f5;
      --shape: #ffffff;
  }
@@ -52,6 +52,42 @@ export const GlobalStyle = createGlobalStyle`
     [disabled]{
         opacity: .6;
         cursor: not-allowed;
+    }
+
+    .react-modal-overlay{
+      background-color: rgba( 0, 0, 0, 0.5);
+      position: fixed;
+      top: 0;
+      bottom: 0;
+      right: 0;
+      left: 0;
+
+      display:flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+    .react-modal-content{
+      width: 100%;
+      max-width: 580px;
+      background: var(--background);
+      padding: 3rem;
+      position: relative;
+      border-radius: 0.25rem
+    }
+
+    .react-modal-close{
+      position: absolute;
+      top: 1.25rem;
+      right: 1.25rem;
+      border: 0;
+      background: transparent;
+
+      transition: filter .2s;
+
+      &:hover{
+        filter: brightness(0.8);
+      }
     }
 
 `;
