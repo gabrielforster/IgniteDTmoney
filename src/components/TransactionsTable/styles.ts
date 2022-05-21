6c25d87feefe08600008c54c99ab1b7bd98326f0
gabrielforster <rochafrgabriel@gmail.com>
2022-05-21 14:00:29 -0300

    feat: change some table styles

--- src/components/TransactionsTable/styles.ts
+++ src/components/TransactionsTable/styles.ts
@@ -1,39 +1,44 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Container = styled.div`
-    margin-top: 4rem;
+  margin-top: 4rem;
 
-    table {
-        width: 100%;
-        border-spacing: 0 0.5rem;
+  table {
+    width: 100%;
+    border-spacing: 0 0.5rem;
 
-        th{
-            color: var(--text-body);
-            font-weight: 400;
-            text-align: left;
-            line-height: 1.5rem;
-            padding: 0 2rem;
-        }
-
-        td{
-            padding: 1rem 2rem;
-            border: 0;
-            background: var(--shape);
-            color: var(--text-body);
-            border-radius: 0.25rem;
-            
-            &:first-child{
-                color: var(--text-title);
-            }
-            
-            &.deposit{
-                color: var(--green);
-            }
-            
-            &.withdraw{
-                color: var(--red)
-            }
-        }
+    th {
+      color: var(--text-body);
+      font-weight: 400;
+      text-align: left;
+      line-height: 1.5rem;
+      padding: 0 2rem;
     }
 
-`
+    td {
+      &:first-child {
+        border-radius: 0.5rem 0 0 0.5rem;
+      }
+      &:last-child {
+        border-radius: 0 0.5rem 0.5rem 0;
+      }
+      padding: 1rem 2rem;
+      border: 0;
+      background: #dcdcdc;
+      color: var(--text-body);
+      /* border-radius: 0.25rem; */
+
+      &:first-child {
+        color: var(--text-title);
+      }
+
+      &.deposit {
+        color: var(--green);
+      }
+
+      &.withdraw {
+        color: var(--red);
+      }
+    }
+  }
+`;
